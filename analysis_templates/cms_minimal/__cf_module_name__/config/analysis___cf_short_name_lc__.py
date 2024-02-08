c2d4682bc8f7fa8bc5f3b279e425d622fc015ef4
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2024-02-08 17:51:29 +0100

    modified template to prepare automatic open data inclusion

--- analysis_templates/cms_minimal/__cf_module_name__/config/analysis___cf_short_name_lc__.py
+++ analysis_templates/cms_minimal/__cf_module_name__/config/analysis___cf_short_name_lc__.py
@@ -3,7 +3,7 @@
 """
 Configuration of the __cf_analysis_name__ analysis.
 """
-
+from __future__ import annotations
 import functools
 
 import law
@@ -16,6 +16,7 @@ from columnflow.config_util import (
     get_root_processes_from_campaign, add_shift_aliases, get_shifts_from_sources, add_category,
     verify_config_processes,
 )
+from columnflow.types import Callable
 
 ak = maybe_import("awkward")
 
@@ -63,280 +64,300 @@ from cmsdb.campaigns.run2_2017_nano_v9 import campaign_run2_2017_nano_v9
 
 # copy the campaign
 # (creates copies of all linked datasets, processes, etc. to allow for encapsulated customization)
-campaign = campaign_run2_2017_nano_v9.copy()
 
-# get all root processes
-procs = get_root_processes_from_campaign(campaign)
+def add_config(
+    analysis: od.Analysis,
+    campaign: od.Campaign,
+    config_name: str | None = None,
+    config_id: int | None = None,
+    limit_dataset_files: int | None = None,
+    get_dataset_lfns: Callable | None = None,
+    dataset_names: list[str] | None = None,
+    get_dataset_lfns_remote_fs: Callable | None = None,
+) -> od.Config:
 
-# create a config by passing the campaign, so id and name will be identical
-cfg = ana.add_config(campaign)
+    # get all root processes
+    procs = get_root_processes_from_campaign(campaign)
 
-# gather campaign data
-year = campaign.x.year
+    # create a config by passing the campaign, so id and name will be identical
+    cfg = analysis.add_config(campaign, name=config_name, id=config_id)
 
-# add processes we are interested in
-process_names = [
-    "data",
-    "tt",
-    "st",
-]
-for process_name in process_names:
-    # add the process
-    proc = cfg.add_process(procs.get(process_name))
+    # gather campaign data
+    year = campaign.x.year
 
-    # configuration of colors, labels, etc. can happen here
-    if proc.is_mc:
-        proc.color1 = (244, 182, 66) if proc.name == "tt" else (244, 93, 66)
+    # add processes we are interested in
+    process_names = [
+        "data",
+        "tt",
+        "st",
+    ]
+    for process_name in process_names:
+        # add the process
+        proc = cfg.add_process(procs.get(process_name))
 
-# add datasets we need to study
-dataset_names = [
-    # data
-    "data_mu_b",
-    # backgrounds
-    "tt_sl_powheg",
-    # signals
-    "st_tchannel_t_powheg",
-]
-for dataset_name in dataset_names:
-    # add the dataset
-    dataset = cfg.add_dataset(campaign.get_dataset(dataset_name))
+        # configuration of colors, labels, etc. can happen here
+        if proc.is_mc:
+            proc.color1 = (244, 182, 66) if proc.name == "tt" else (244, 93, 66)
 
-    # for testing purposes, limit the number of files to 2
-    for info in dataset.info.values():
-        info.n_files = min(info.n_files, 2)
+    # add datasets we need to study
+    if not dataset_names:
+        dataset_names = [
+            # data
+            "data_mu_b",
+            # backgrounds
+            "tt_sl_powheg",
+            # signals
+            "st_tchannel_t_powheg",
+        ]
+    for dataset_name in dataset_names:
+        # add the dataset
+        dataset = cfg.add_dataset(campaign.get_dataset(dataset_name))
 
-# verify that the root process of all datasets is part of any of the registered processes
-verify_config_processes(cfg, warn=True)
+        # for testing purposes, limit the number of files
+        if limit_dataset_files is not None:
+            for info in dataset.info.values():
+                info.n_files = min(info.n_files, limit_dataset_files)
 
-# default objects, such as calibrator, selector, producer, ml model, inference model, etc
-cfg.x.default_calibrator = "example"
-cfg.x.default_selector = "example"
-cfg.x.default_producer = "example"
-cfg.x.default_ml_model = None
-cfg.x.default_inference_model = "example"
-cfg.x.default_categories = ("incl",)
-cfg.x.default_variables = ("n_jet", "jet1_pt")
+    # verify that the root process of all datasets is part of any of the registered processes
+    verify_config_processes(cfg, warn=True)
 
-# process groups for conveniently looping over certain processs
-# (used in wrapper_factory and during plotting)
-cfg.x.process_groups = {}
+    # default objects, such as calibrator, selector, producer, ml model, inference model, etc
+    cfg.x.default_calibrator = "example"
+    cfg.x.default_selector = "example"
+    cfg.x.default_producer = "example"
+    cfg.x.default_ml_model = None
+    cfg.x.default_inference_model = "example"
+    cfg.x.default_categories = ("incl",)
+    cfg.x.default_variables = ("n_jet", "jet1_pt")
 
-# dataset groups for conveniently looping over certain datasets
-# (used in wrapper_factory and during plotting)
-cfg.x.dataset_groups = {}
+    # process groups for conveniently looping over certain processs
+    # (used in wrapper_factory and during plotting)
+    cfg.x.process_groups = {}
 
-# category groups for conveniently looping over certain categories
-# (used during plotting)
-cfg.x.category_groups = {}
+    # dataset groups for conveniently looping over certain datasets
+    # (used in wrapper_factory and during plotting)
+    cfg.x.dataset_groups = {}
 
-# variable groups for conveniently looping over certain variables
-# (used during plotting)
-cfg.x.variable_groups = {}
+    # category groups for conveniently looping over certain categories
+    # (used during plotting)
+    cfg.x.category_groups = {}
 
-# shift groups for conveniently looping over certain shifts
-# (used during plotting)
-cfg.x.shift_groups = {}
+    # variable groups for conveniently looping over certain variables
+    # (used during plotting)
+    cfg.x.variable_groups = {}
 
-# selector step groups for conveniently looping over certain steps
-# (used in cutflow tasks)
-cfg.x.selector_step_groups = {
-    "default": ["muon", "jet"],
-}
+    # shift groups for conveniently looping over certain shifts
+    # (used during plotting)
+    cfg.x.shift_groups = {}
 
-# custom method and sandbox for determining dataset lfns
-cfg.x.get_dataset_lfns = None
-cfg.x.get_dataset_lfns_sandbox = None
+    # selector step groups for conveniently looping over certain steps
+    # (used in cutflow tasks)
+    cfg.x.selector_step_groups = {
+        "default": ["muon", "jet"],
+    }
 
-# whether to validate the number of obtained LFNs in GetDatasetLFNs
-# (currently set to false because the number of files per dataset is truncated to 2)
-cfg.x.validate_dataset_lfns = False
+    # custom method and sandbox for determining dataset lfns
+    cfg.x.get_dataset_lfns = get_dataset_lfns
+    cfg.x.get_dataset_lfns_sandbox = None
+    cfg.x.get_dataset_lfns_remote_fs = get_dataset_lfns_remote_fs
 
-# lumi values in inverse pb
-# https://twiki.cern.ch/twiki/bin/view/CMS/LumiRecommendationsRun2?rev=2#Combination_and_correlations
-cfg.x.luminosity = Number(41480, {
-    "lumi_13TeV_2017": 0.02j,
-    "lumi_13TeV_1718": 0.006j,
-    "lumi_13TeV_correlated": 0.009j,
-})
+    # whether to validate the number of obtained LFNs in GetDatasetLFNs
+    # (currently set to false because the number of files per dataset is truncated to 2)
+    cfg.x.validate_dataset_lfns = False
 
-# names of muon correction sets and working points
-# (used in the muon producer)
-cfg.x.muon_sf_names = ("NUM_TightRelIso_DEN_TightIDandIPCut", f"{year}_UL")
+    # lumi values in inverse pb
+    # https://twiki.cern.ch/twiki/bin/view/CMS/LumiRecommendationsRun2?rev=2#Combination_and_correlations
+    cfg.x.luminosity = Number(41480, {
+        "lumi_13TeV_2017": 0.02j,
+        "lumi_13TeV_1718": 0.006j,
+        "lumi_13TeV_correlated": 0.009j,
+    })
 
-# register shifts
-cfg.add_shift(name="nominal", id=0)
+    # names of muon correction sets and working points
+    # (used in the muon producer)
+    cfg.x.muon_sf_names = ("NUM_TightRelIso_DEN_TightIDandIPCut", f"{year}_UL")
 
-# tune shifts are covered by dedicated, varied datasets, so tag the shift as "disjoint_from_nominal"
-# (this is currently used to decide whether ML evaluations are done on the full shifted dataset)
-cfg.add_shift(name="tune_up", id=1, type="shape", tags={"disjoint_from_nominal"})
-cfg.add_shift(name="tune_down", id=2, type="shape", tags={"disjoint_from_nominal"})
+    # register shifts
+    cfg.add_shift(name="nominal", id=0)
 
-# fake jet energy correction shift, with aliases flaged as "selection_dependent", i.e. the aliases
-# affect columns that might change the output of the event selection
-cfg.add_shift(name="jec_up", id=20, type="shape")
-cfg.add_shift(name="jec_down", id=21, type="shape")
-add_shift_aliases(
-    cfg,
-    "jec",
-    {
-        "Jet.pt": "Jet.pt_{name}",
-        "Jet.mass": "Jet.mass_{name}",
-        "MET.pt": "MET.pt_{name}",
-        "MET.phi": "MET.phi_{name}",
-    },
-)
+    # tune shifts are covered by dedicated, varied datasets, so tag the shift as "disjoint_from_nominal"
+    # (this is currently used to decide whether ML evaluations are done on the full shifted dataset)
+    cfg.add_shift(name="tune_up", id=1, type="shape", tags={"disjoint_from_nominal"})
+    cfg.add_shift(name="tune_down", id=2, type="shape", tags={"disjoint_from_nominal"})
 
-# event weights due to muon scale factors
-cfg.add_shift(name="mu_up", id=10, type="shape")
-cfg.add_shift(name="mu_down", id=11, type="shape")
-add_shift_aliases(cfg, "mu", {"muon_weight": "muon_weight_{direction}"})
+    # fake jet energy correction shift, with aliases flaged as "selection_dependent", i.e. the aliases
+    # affect columns that might change the output of the event selection
+    cfg.add_shift(name="jec_up", id=20, type="shape")
+    cfg.add_shift(name="jec_down", id=21, type="shape")
+    add_shift_aliases(
+        cfg,
+        "jec",
+        {
+            "Jet.pt": "Jet.pt_{name}",
+            "Jet.mass": "Jet.mass_{name}",
+            "MET.pt": "MET.pt_{name}",
+            "MET.phi": "MET.phi_{name}",
+        },
+    )
 
-# external files
-json_mirror = "/afs/cern.ch/work/m/mrieger/public/mirrors/jsonpog-integration-9ea86c4c"
-cfg.x.external_files = DotDict.wrap({
-    # lumi files
-    "lumi": {
-        "golden": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", "v1"),  # noqa
-        "normtag": ("/afs/cern.ch/user/l/lumipro/public/Normtags/normtag_PHYSICS.json", "v1"),
-    },
+    # event weights due to muon scale factors
+    cfg.add_shift(name="mu_up", id=10, type="shape")
+    cfg.add_shift(name="mu_down", id=11, type="shape")
+    add_shift_aliases(cfg, "mu", {"muon_weight": "muon_weight_{direction}"})
 
-    # muon scale factors
-    "muon_sf": (f"{json_mirror}/POG/MUO/{year}_UL/muon_Z.json.gz", "v1"),
-})
+    # external files
+    json_mirror = "/afs/cern.ch/work/m/mrieger/public/mirrors/jsonpog-integration-9ea86c4c"
+    cfg.x.external_files = DotDict.wrap({
+        # lumi files
+        "lumi": {
+            "golden": ("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", "v1"),  # noqa
+            "normtag": ("/afs/cern.ch/user/l/lumipro/public/Normtags/normtag_PHYSICS.json", "v1"),
+        },
 
-# target file size after MergeReducedEvents in MB
-cfg.x.reduced_file_size = 512.0
+        # muon scale factors
+        "muon_sf": (f"{json_mirror}/POG/MUO/{year}_UL/muon_Z.json.gz", "v1"),
+    })
 
-# columns to keep after certain steps
-cfg.x.keep_columns = DotDict.wrap({
-    "cf.ReduceEvents": {
-        # general event info, mandatory for reading files with coffea
-        ColumnCollection.MANDATORY_COFFEA,
-        # object info
-        "Jet.pt", "Jet.eta", "Jet.phi", "Jet.mass", "Jet.btagDeepFlavB", "Jet.hadronFlavour",
-        "Muon.pt", "Muon.eta", "Muon.phi", "Muon.mass", "Muon.pfRelIso04_all",
-        "MET.pt", "MET.phi", "MET.significance", "MET.covXX", "MET.covXY", "MET.covYY",
-        "PV.npvs",
-        # all columns added during selection using a ColumnCollection flag
-        ColumnCollection.ALL_FROM_SELECTOR,
-    },
-    "cf.MergeSelectionMasks": {
-        "cutflow.*",
-    },
-    "cf.UniteColumns": {
-        "*",
-    },
-})
+    # target file size after MergeReducedEvents in MB
+    cfg.x.reduced_file_size = 512.0
 
-# event weight columns as keys in an OrderedDict, mapped to shift instances they depend on
-get_shifts = functools.partial(get_shifts_from_sources, cfg)
-cfg.x.event_weights = DotDict({
-    "normalization_weight": [],
-    "muon_weight": get_shifts("mu"),
-})
+    # columns to keep after certain steps
+    cfg.x.keep_columns = DotDict.wrap({
+        "cf.ReduceEvents": {
+            # general event info, mandatory for reading files with coffea
+            ColumnCollection.MANDATORY_COFFEA,
+            # object info
+            "Jet.pt", "Jet.eta", "Jet.phi", "Jet.mass", "Jet.btagDeepFlavB", "Jet.hadronFlavour",
+            "Muon.pt", "Muon.eta", "Muon.phi", "Muon.mass", "Muon.pfRelIso04_all",
+            "MET.pt", "MET.phi", "MET.significance", "MET.covXX", "MET.covXY", "MET.covYY",
+            "PV.npvs",
+            # all columns added during selection using a ColumnCollection flag
+            ColumnCollection.ALL_FROM_SELECTOR,
+        },
+        "cf.MergeSelectionMasks": {
+            "cutflow.*",
+        },
+        "cf.UniteColumns": {
+            "*",
+        },
+    })
 
-# versions per task family, either referring to strings or to callables receving the invoking
-# task instance and parameters to be passed to the task family
-cfg.x.versions = {
-    # "cf.CalibrateEvents": "prod1",
-    # "cf.SelectEvents": (lambda cls, inst, params: "prod1" if params.get("selector") == "default" else "dev1"),
-    # ...
-}
+    # event weight columns as keys in an OrderedDict, mapped to shift instances they depend on
+    get_shifts = functools.partial(get_shifts_from_sources, cfg)
+    cfg.x.event_weights = DotDict({
+        "normalization_weight": [],
+        "muon_weight": get_shifts("mu"),
+    })
 
-# channels
-# (just one for now)
-cfg.add_channel(name="mutau", id=1)
+    # versions per task family, either referring to strings or to callables receving the invoking
+    # task instance and parameters to be passed to the task family
+    cfg.x.versions = {
+        # "cf.CalibrateEvents": "prod1",
+        # "cf.SelectEvents": (lambda cls, inst, params: "prod1" if params.get("selector") == "default" else "dev1"),
+        # ...
+    }
 
-# add categories using the "add_category" tool which adds auto-generated ids
-# the "selection" entries refer to names of selectors, e.g. in selection/example.py
-# note: it is recommended to always add an inclusive category with id=1 or name="incl" which is used
-#       in various places, e.g. for the inclusive cutflow plots and the "empty" selector
-add_category(
-    cfg,
-    id=1,
-    name="incl",
-    selection="cat_incl",
-    label="inclusive",
-)
-add_category(
-    cfg,
-    name="2j",
-    selection="cat_2j",
-    label="2 jets",
-)
+    # channels
+    # (just one for now)
+    cfg.add_channel(name="mutau", id=1)
 
-# add variables
-# (the "event", "run" and "lumi" variables are required for some cutflow plotting task,
-# and also correspond to the minimal set of columns that coffea's nano scheme requires)
-cfg.add_variable(
-    name="event",
-    expression="event",
-    binning=(1, 0.0, 1.0e9),
-    x_title="Event number",
-    discrete_x=True,
-)
-cfg.add_variable(
-    name="run",
-    expression="run",
-    binning=(1, 100000.0, 500000.0),
-    x_title="Run number",
-    discrete_x=True,
-)
-cfg.add_variable(
-    name="lumi",
-    expression="luminosityBlock",
-    binning=(1, 0.0, 5000.0),
-    x_title="Luminosity block",
-    discrete_x=True,
-)
-cfg.add_variable(
-    name="n_jet",
-    expression="n_jet",
-    binning=(11, -0.5, 10.5),
-    x_title="Number of jets",
-    discrete_x=True,
-)
-cfg.add_variable(
-    name="jets_pt",
-    expression="Jet.pt",
-    binning=(40, 0.0, 400.0),
-    unit="GeV",
-    x_title=r"$p_{T} of all jets$",
-)
-cfg.add_variable(
-    name="jet1_pt",
-    expression="Jet.pt[:,0]",
-    null_value=EMPTY_FLOAT,
-    binning=(40, 0.0, 400.0),
-    unit="GeV",
-    x_title=r"Jet 1 $p_{T}$",
-)
-cfg.add_variable(
-    name="jet1_eta",
-    expression="Jet.eta[:,0]",
-    null_value=EMPTY_FLOAT,
-    binning=(30, -3.0, 3.0),
-    x_title=r"Jet 1 $\eta$",
-)
-cfg.add_variable(
-    name="ht",
-    expression=lambda events: ak.sum(events.Jet.pt, axis=1),
-    binning=(40, 0.0, 800.0),
-    unit="GeV",
-    x_title="HT",
-)
-# weights
-cfg.add_variable(
-    name="mc_weight",
-    expression="mc_weight",
-    binning=(200, -10, 10),
-    x_title="MC weight",
-)
-# cutflow variables
-cfg.add_variable(
-    name="cf_jet1_pt",
-    expression="cutflow.jet1_pt",
-    binning=(40, 0.0, 400.0),
-    unit="GeV",
-    x_title=r"Jet 1 $p_{T}$",
+    # add categories using the "add_category" tool which adds auto-generated ids
+    # the "selection" entries refer to names of selectors, e.g. in selection/example.py
+    # note: it is recommended to always add an inclusive category with id=1 or name="incl" which is used
+    #       in various places, e.g. for the inclusive cutflow plots and the "empty" selector
+    add_category(
+        cfg,
+        id=1,
+        name="incl",
+        selection="cat_incl",
+        label="inclusive",
+    )
+    add_category(
+        cfg,
+        name="2j",
+        selection="cat_2j",
+        label="2 jets",
+    )
+
+    # add variables
+    # (the "event", "run" and "lumi" variables are required for some cutflow plotting task,
+    # and also correspond to the minimal set of columns that coffea's nano scheme requires)
+    cfg.add_variable(
+        name="event",
+        expression="event",
+        binning=(1, 0.0, 1.0e9),
+        x_title="Event number",
+        discrete_x=True,
+    )
+    cfg.add_variable(
+        name="run",
+        expression="run",
+        binning=(1, 100000.0, 500000.0),
+        x_title="Run number",
+        discrete_x=True,
+    )
+    cfg.add_variable(
+        name="lumi",
+        expression="luminosityBlock",
+        binning=(1, 0.0, 5000.0),
+        x_title="Luminosity block",
+        discrete_x=True,
+    )
+    cfg.add_variable(
+        name="n_jet",
+        expression="n_jet",
+        binning=(11, -0.5, 10.5),
+        x_title="Number of jets",
+        discrete_x=True,
+    )
+    cfg.add_variable(
+        name="jets_pt",
+        expression="Jet.pt",
+        binning=(40, 0.0, 400.0),
+        unit="GeV",
+        x_title=r"$p_{T} of all jets$",
+    )
+    cfg.add_variable(
+        name="jet1_pt",
+        expression="Jet.pt[:,0]",
+        null_value=EMPTY_FLOAT,
+        binning=(40, 0.0, 400.0),
+        unit="GeV",
+        x_title=r"Jet 1 $p_{T}$",
+    )
+    cfg.add_variable(
+        name="jet1_eta",
+        expression="Jet.eta[:,0]",
+        null_value=EMPTY_FLOAT,
+        binning=(30, -3.0, 3.0),
+        x_title=r"Jet 1 $\eta$",
+    )
+    cfg.add_variable(
+        name="ht",
+        expression=lambda events: ak.sum(events.Jet.pt, axis=1),
+        binning=(40, 0.0, 800.0),
+        unit="GeV",
+        x_title="HT",
+    )
+    # weights
+    cfg.add_variable(
+        name="mc_weight",
+        expression="mc_weight",
+        binning=(200, -10, 10),
+        x_title="MC weight",
+    )
+    # cutflow variables
+    cfg.add_variable(
+        name="cf_jet1_pt",
+        expression="cutflow.jet1_pt",
+        binning=(40, 0.0, 400.0),
+        unit="GeV",
+        x_title=r"Jet 1 $p_{T}$",
+    )
+
+add_config(
+    ana,
+    campaign_run2_2017_nano_v9.copy(),
+    config_name=campaign_run2_2017_nano_v9.name,
+    config_id=1
 )
---
+++ tests/workflow_data/config_test_files.py
@@ -0,0 +1,134 @@
+# coding: utf-8
+
+"""
+Campaign definitions and datasets published as part of the CMS OpenData 2015 initiative
+as used by the Analysis Grand Challenge.
+"""
+
+from __future__ import annotations
+
+import os
+import json
+import re
+
+from order import Campaign, DatasetInfo, Dataset, Shift
+
+import processes as procs
+
+
+#
+# campaign
+#
+
+campaign_cms_opendata_2015_agc = cpn = Campaign(
+    name="cms_opendata_2015_agc",
+    id=1,
+    ecm=13,
+    aux={
+        "tier": "NanoAOD",
+        "year": 2015,
+        "location": "https://xrootd-local.unl.edu:1094//store/user/AGC/nanoAOD",
+        "wlcg_fs": "wlcg_fs_unl",
+    },
+)
+
+
+#
+# helpers for working with the agc file list
+#
+
+# load the agc file list
+# located at https://raw.githubusercontent.com/iris-hep/analysis-grand-challenge/main/analyses/cms-open-data-ttbar/nanoaod_inputs.json
+agc_files_path = "./nanoaod_inputs.json"
+with open(os.path.expandvars(agc_files_path), "r") as f:
+    agc_files = json.load(f)
+
+# customization of the lfn retrieval in GetDatasetLFNs to detect files in the agc file list
+def get_dataset_lfns(
+    dataset_inst: Dataset,
+    shift_inst: Shift,
+    dataset_key: str,
+) -> list[str]:
+    # get process and systematic names as used by the agc
+    agc_process = dataset_inst.x.agc_process
+    agc_syst = dataset_inst.x("agc_shifts", {}).get(shift_inst.name, shift_inst.name)
+    # retrieve and return data
+    return [
+        re.match("^https?://.*(/store/.+)$", data["path"]).group(1)
+        for data in agc_files[agc_process][agc_syst]["files"]
+    ]
+
+get_dataset_lfns_remote_fs = lambda dataset_inst: campaign_cms_opendata_2015_agc.x.wlcg_fs
+
+def get_dataset_info(process: str, syst: str) -> dict[str, list[str] | int]:
+    # interpret the dataset "key" as the fragment after "/store/user/AGC/nanoAOD" of the first file
+    return {
+        "keys": [agc_files[process][syst]["files"][0]["path"].split("/")[8]],
+        "n_files": len(agc_files[process][syst]["files"]),
+        "n_events": agc_files[process][syst]["nevts_total"],
+    }
+
+
+#
+# datasets
+# (ids are pretty random, they would normally refer to IDs used in central databases)
+#
+
+cpn.add_dataset(
+    name="tt_powheg",
+    id=1,
+    processes=[procs.tt],
+    info={
+        "nominal": DatasetInfo(**get_dataset_info("ttbar", "nominal")),
+        "scale_down": DatasetInfo(**get_dataset_info("ttbar", "scaledown")),
+        "scale_up": DatasetInfo(**get_dataset_info("ttbar", "scaleup")),
+        # TODO: ME, PS
+    },
+    aux={
+        "agc_process": "ttbar",
+        "agc_shifts": {
+            "scale_down": "scaledown",
+            "scale_up": "scaleup",
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="st_schannel_amcatnlo",
+    id=2,
+    processes=[procs.st_schannel],
+    aux={
+        "agc_process": "single_top_s_chan",
+    },
+    **get_dataset_info("single_top_s_chan", "nominal"),
+)
+
+cpn.add_dataset(
+    name="st_tchannel_powheg",
+    id=3,
+    processes=[procs.st_tchannel],
+    aux={
+        "agc_process": "single_top_t_chan",
+    },
+    **get_dataset_info("single_top_t_chan", "nominal"),
+)
+
+cpn.add_dataset(
+    name="st_twchannel_powheg",
+    id=4,
+    processes=[procs.st_twchannel],
+    aux={
+        "agc_process": "single_top_tW",
+    },
+    **get_dataset_info("single_top_tW", "nominal"),
+)
+
+cpn.add_dataset(
+    name="wjets_amcatnlo",
+    id=5,
+    processes=[procs.w],
+    aux={
+        "agc_process": "wjets",
+    },
+    **get_dataset_info("wjets", "nominal"),
+)
